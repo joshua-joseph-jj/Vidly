bf6ff01dd4de8393f0ad52e61c72d9bd9ad211a8
Joshua Joseph <joshuajoseph07@gmail.com>
2020-05-28 23:50:19 -0400

    populated movies table

--- Models/Movie.cs
+++ Models/Movie.cs
@@ -3,13 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
 
 namespace Vidly.Models
 {
     public class Movie
     {
+        
         public int Id { get; set; }
+
+        [Required]
         public string Name { get; set; }
+
+        [Required]
         public string Genre { get; set; }
 
 
@@ -21,6 +27,7 @@ namespace Vidly.Models
         public DateTime DateAdded { get; set; }
 
 
+        [Required]
         public int NumberInStock { get; set; }
 
 
